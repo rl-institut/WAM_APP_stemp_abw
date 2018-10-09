fcea51349a0df967ac6d9c51432b191c6a8c6d46
nesnoj <jonathan.amme@rl-institut.de>
2018-10-09 18:14:35 +0200

    add OpenTopoMap layer

--- settings.py
+++ settings.py
@@ -20,6 +20,10 @@ LEAFLET_CONFIG = {
               ('OSM', 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                {'attribution': '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
                 'minZoom': 9,
+                }),
+              ('OpenTopoMap', 'http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+               {'attribution': 'Data: &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Display: &copy <a href="http://opentopomap.org">OpenTopoMap</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+                'minZoom': 9,
                 })
               ],
     'DEFAULT_CENTER': (51.834167, 12.237778),
