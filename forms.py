71a5ac5ade8a4d9fce2118864a8429461f410cab
nesnoj <jonathan.amme@rl-institut.de>
2018-12-14 14:15:40 +0100

    dynamic components: add new group form and component widget

--- forms.py
+++ forms.py
@@ -1,25 +1,20 @@
 from django import forms
 
-from .widgets import LayerSelectWidget
+from .widgets import LayerSelectWidget, SliderWidget
 
-
-class LayerSelectForm(forms.Form):
+class LayerGroupForm(forms.Form):
     """Form for layer group"""
 
     def __init__(self, layers=None, *args, **kwargs):
         if layers is None:
             raise ValueError('No layers given. '
                              'Please add some in layers.cfg.')
-        super(LayerSelectForm, self).__init__(*args, **kwargs)
+        super(LayerGroupForm, self).__init__(*args, **kwargs)
 
         for name, data in layers.items():
             self.fields[name] = forms.TypedChoiceField(
                 #label = 'Layer {}'.format(name),
                 label = '',
-                # choices = (
-                #     (1, "Yes"),
-                #     (0, "No")
-                # ),
                 coerce = lambda x: bool(int(x)),
                 widget=LayerSelectWidget(
                     attrs={'id': 'cb_{}'.format(name),
@@ -31,6 +26,36 @@ class LayerSelectForm(forms.Form):
                            'checked': True if data['show'] == '1' else False
                            }
                 ),
-                #initial = 1,
-                required = False
+                required=False
             )
+
+
+class ComponentGroupForm(forms.Form):
+    """Form for esys components"""
+
+    def __init__(self, components=None, *args, **kwargs):
+        if components is None:
+            raise ValueError('No components given. '
+                             'Please add some in esys_components.cfg.')
+        super(ComponentGroupForm, self).__init__(*args, **kwargs)
+        # self.helper = FormHelper(self)
+        # self.helper.template = 'forms/parameter_form.html'
+
+        for name, data in components.items():
+            if data['type'] == 'range':
+                self.fields[name] = forms.FloatField(
+                    label='sl_{}'.format(name),
+                    widget=SliderWidget(
+                        attrs={'id': 'sl_{}'.format(name),
+                               'title': data['title'],
+                               'text': data['text'],
+                               'min': data['min'],
+                               'max': data['max'],
+                               'from': data['value'],
+                               'step': data['step'],
+                               #'name': name
+                               }
+                    ),
+                    #initial = 1,
+                    required = False
+                )
--- widgets.py
+++ widgets.py
@@ -1,4 +1,4 @@
-from django.forms.widgets import CheckboxInput
+from django.forms.widgets import CheckboxInput, NumberInput
 
 
 class LayerSelectWidget(CheckboxInput):
@@ -6,3 +6,24 @@ class LayerSelectWidget(CheckboxInput):
 
     # def __init__(self, attrs=None):
     #     super(CheckboxInput, self).__init__(attrs)
+
+
+class SliderWidget(NumberInput):
+    input_type = 'number'
+    template_name = 'widgets/esys_component.html'
+
+    # def __init__(self, step_size=1, attrs=None):
+    #     super(SliderWidget, self).__init__(attrs)
+    #     self.step_size = step_size
+    #
+    # def __get_precision(self):
+    #     try:
+    #         return len(str(self.step_size).split('.')[1])
+    #     except IndexError:
+    #         return 0
+    #
+    # def get_context(self, name, value, attrs):
+    #     context = super(SliderWidget, self).get_context(name, value, attrs)
+    #     context['widget']['step_size'] = self.step_size
+    #     #context['widget']['precision'] = self.__get_precision()
+    #     return context
