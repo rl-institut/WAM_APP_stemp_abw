b82072697bcaa76c46c9d902d9236815a6304529
nesnoj <jonathan.amme@rl-institut.de>
2019-02-28 17:49:22 +0100

    rm temporary solution to import app-apecific settings
    fixed in https://github.com/rl-institut/WAM/issues/14

--- __init__.py
+++
@@ -1,5 +0,0 @@
-default_app_config = 'stemp_abw.apps.StempAbwConfig'
-
-# Todo: this is temporary solution to import app-apecific settings
-# It will be read by the WAM settings in the future
-from stemp_abw import app_settings
