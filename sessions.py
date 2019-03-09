bcff6a369dfb3284c2e0458459ce0ff108ad4628
nesnoj <jonathan.amme@rl-institut.de>
2019-03-09 03:20:06 +0100

    load non-user scenarios into UserSession at startup

--- sessions.py
+++ sessions.py
@@ -1,8 +1,16 @@
+import json
+
+from stemp_abw.models import Scenario
+
 
 class UserSession(object):
     def __init__(self):
-        self.scenarios = []
+        self.scenarios = self.init_default_scenarios()
         self.simulation = Simulation()
+
+    def init_default_scenarios(self):
+        scns = Scenario.objects.filter(is_user_scenario=False).all()
+        return {scn.name: scn for scn in scns}
 
 
 class Simulation(object):
