01b9be682c1ad2f7e9372b0c93636ea3e9264558
nesnoj <jonathan.amme@rl-institut.de>
2019-03-13 14:24:13 +0100

    init user scenario using status quo

--- sessions.py
+++ sessions.py
@@ -1,10 +1,11 @@
-import json
+from uuid import uuid4
 
 from stemp_abw.models import Scenario
 
 
 class UserSession(object):
     def __init__(self):
+        self.user_scenario = self.__init_user_scenario()
         self.simulation = Simulation()
 
     @property
@@ -14,6 +15,16 @@ class UserSession(object):
                     is_user_scenario=False).all()
                 }
 
+    @staticmethod
+    def __init_user_scenario():
+        """Make a copy of status quo scenario for user scenario"""
+        scn = Scenario.objects.get(name='Status quo')
+        scn.name = 'User Scenario {uuid}'.format(uuid=str(uuid4()))
+        scn.description = ''
+        scn.id = None
+        # scn.save()
+        return scn
+    
 
 class Simulation(object):
     def __init__(self):
