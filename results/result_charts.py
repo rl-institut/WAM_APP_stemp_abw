5f0d2ad67341944bcbee4e76cb2a888120880ea3
nesnoj <jonathan.amme@rl-institut.de>
2019-06-07 12:50:54 +0200

    add first bunch of result charts #36

--- results/result_charts.py
+++ results/result_charts.py
@@ -1,76 +1,5 @@
 from stemp_abw.results import results
-
-# TODO: Temp stuff for WS
-# LineChart: used in results as placeholder charts
-
-
-
-# TODO: Temp stuff for WS
-# LineChart: used in results as placeholder charts
-labels2 = [
-    {
-        'setup_labels': {
-            'title': {'text': 'Erzeugung'},
-            'subtitle': {'text': 'in GW'},
-            'yAxis': {'title': {'text': 'GW'}}
-        },
-        'data_labels': ['Strom', 'Wärme']
-    },
-    {
-        'setup_labels': {
-            'title': {'text': 'Bedarf'},
-            'subtitle': {'text': 'in GW'},
-            'yAxis': {'title': {'text': 'GW'}}
-        },
-        'data_labels': ['Strom', 'Wärme']
-    },
-    {
-        'setup_labels': {
-            'title': {'text': 'Erneuerbare Energien'},
-            'subtitle': {'text': 'in GW'},
-            'yAxis': {'title': {'text': 'GW'}}
-        },
-        'data_labels': ['Wind', 'Solar']
-    }
-]
-visualizations2 = [results.ResultAnalysisVisualization(
-    setup_labels=l['setup_labels'],
-    data_labels=l['data_labels'],
-    type='line').visualize() for l in labels2]
-
-
-# TODO: Temp stuff for WS
-# StackedGroupedColumnChart: used in popups as placeholder charts
-labels5 = [
-    {
-        'setup_labels': {
-            'title': {'text': 'Erzeugung'},
-            'subtitle': {'text': 'in GW'},
-            'yAxis': {'title': {'text': 'GW'}}
-        },
-        'data_labels': ['Anhalt', 'Bitterfeld', 'Wolfen', 'Dessau', 'Zerbst']
-    },
-    {
-        'setup_labels': {
-            'title': {'text': 'Bedarf'},
-            'subtitle': {'text': 'in GW'},
-            'yAxis': {'title': {'text': 'GW'}}
-        },
-        'data_labels': ['Anhalt', 'Bitterfeld', 'Wolfen', 'Dessau', 'Zerbst']
-    },
-    {
-        'setup_labels': {
-            'title': {'text': 'Erneuerbare Energien'},
-            'subtitle': {'text': 'in GW'},
-            'yAxis': {'title': {'text': 'GW'}}
-        },
-        'data_labels': ['Anhalt', 'Bitterfeld', 'Wolfen', 'Dessau', 'Zerbst']
-    }
-]
-visualizations5 = [results.ResultAnalysisVisualization(
-    setup_labels=l['setup_labels'],
-    data_labels=l['data_labels'],
-    type='column').visualize() for l in labels5]
+import pandas as pd
 
 
 ####################
@@ -79,29 +8,69 @@ visualizations5 = [results.ResultAnalysisVisualization(
 
 results_charts_tab1 = [
     {
-        'container_id': 'hc_res_summary_scn',
-        'type': 'pie',
+        'container_id': 'hc_column_power_prod_both_scn',
+        'type': 'column',
         'setup_labels': {
-            'title': {'text': 'Ihr Szenario'},
+            'title': {'text': 'Energieerzeugung Strom'},
             'subtitle': {'text': 'in GWh'},
             'yAxis': {'title': {'text': 'GWh'}}
-        }
+        },
+        'data': pd.DataFrame(data=[{'Windenergie': 0,
+                                    'PV Freifläche': 0,
+                                    'PV Dach': 0,
+                                    'Wasserkraft': 0,
+                                    'Biogas': 0,
+                                    'Fossile Brennstoffe': 0,
+                                    'Import': 0},
+                                   {'Windenergie': 0,
+                                    'PV Freifläche': 0,
+                                    'PV Dach': 0,
+                                    'Wasserkraft': 0,
+                                    'Biogas': 0,
+                                    'Fossile Brennstoffe': 0,
+                                    'Import': 0}
+                                   ],
+                             index=['Ihr Szenario', 'Status quo'])
     },
     {
-        'container_id': 'hc_res_summary_sq',
-        'type': 'pie',
+        'container_id': 'hc_column_power_dem_both_scn',
+        'type': 'column',
         'setup_labels': {
-            'title': {'text': 'Status quo'},
+            'title': {'text': 'Energieverbrauch Strom'},
             'subtitle': {'text': 'in GWh'},
             'yAxis': {'title': {'text': 'GWh'}}
-        }
+        },
+        'data': pd.DataFrame(data=[{'Haushalte': 0,
+                                    'GHD': 0,
+                                    'Industrie': 0,
+                                    'Export': 0},
+                                   {'Haushalte': 0,
+                                    'GHD': 0,
+                                    'Industrie': 0,
+                                    'Export': 0}
+                                   ],
+                             index=['Ihr Szenario', 'Status quo'])
+    },
+    {
+        'container_id': 'hc_column_power_own_cons_both_scn',
+        'type': 'column',
+        'setup_labels': {
+            'title': {'text': 'Eigenversorgung Strom (bilanziell)'},
+            'subtitle': {'text': 'in %'},
+            'yAxis': {'title': {'text': '%'}}
+        },
+        'data': pd.DataFrame(data=[{'Eigenversorgung': 0},
+                                   {'Eigenversorgung': 0}
+                                   ],
+                             index=['Ihr Szenario', 'Status quo'])
     }
 ]
 
-results_charts_tab1_viz = [results.ResultAnalysisVisualization(
+results_charts_tab1_viz = [results.ResultChart(
     setup_labels=chart['setup_labels'],
-    type=chart['type']).visualize(renderTo=chart['container_id'])
-                   for chart in results_charts_tab1]
+    type=chart['type'],
+    data=chart['data']).visualize(renderTo=chart['container_id'])
+                           for chart in results_charts_tab1]
 
 ####################
 ### Result Tab 2 ###
@@ -109,36 +78,84 @@ results_charts_tab1_viz = [results.ResultAnalysisVisualization(
 
 results_charts_tab2 = [
     {
-        'container_id': 'hc_res_production_scn',
+        'container_id': 'hc_pie_power_production_user_scn',
         'type': 'pie',
         'setup_labels': {
             'title': {'text': 'Ihr Szenario'},
             'subtitle': {'text': 'in GWh'},
             'yAxis': {'title': {'text': 'GWh'}}
-        }
+        },
+        'data': pd.DataFrame(data={'name': ['Windenergie',
+                                            'PV Freifläche',
+                                            'PV Dach',
+                                            'Wasserkraft',
+                                            'Biogas',
+                                            'Fossile Brennstoffe',
+                                            'Import'],
+                                   'y': [0] * 7}).reset_index().to_dict(orient='records')
     },
     {
-        'container_id': 'hc_res_production_sq',
+        'container_id': 'hc_pie_power_production_sq_scn',
         'type': 'pie',
         'setup_labels': {
             'title': {'text': 'Status quo'},
             'subtitle': {'text': 'in GWh'},
             'yAxis': {'title': {'text': 'GWh'}}
-        }
+        },
+        'data': pd.DataFrame(data={'name': ['Windenergie',
+                                            'PV Freifläche',
+                                            'PV Dach',
+                                            'Wasserkraft',
+                                            'Biogas',
+                                            'Fossile Brennstoffe',
+                                            'Import'],
+                                   'y': [0] * 7}).reset_index().to_dict(orient='records')
     }
 ]
 
-results_charts_tab2_viz = [results.ResultAnalysisVisualization(
+results_charts_tab2_viz = [results.ResultChart(
     setup_labels=chart['setup_labels'],
-    type=chart['type']).visualize(renderTo=chart['container_id'])
-                   for chart in results_charts_tab2]
-
+    type=chart['type'],
+    data=chart['data']).visualize(renderTo=chart['container_id'])
+                           for chart in results_charts_tab2]
 
 ####################
 ### Result Tab 3 ###
 ####################
 
 results_charts_tab3 = [
+    {
+        'container_id': 'hc_column_power_dem_both_scn2',
+        'type': 'column',
+        'setup_labels': {
+            'title': {'text': 'Energieverbrauch (Strom)'},
+            'subtitle': {'text': 'in GWh'},
+            'yAxis': {'title': {'text': 'GWh'}}
+        },
+        'data': pd.DataFrame(data=[{'Haushalte': 0,
+                                    'GHD': 0,
+                                    'Industrie': 0,
+                                    'Export': 0},
+                                   {'Haushalte': 0,
+                                    'GHD': 0,
+                                    'Industrie': 0,
+                                    'Export': 0}
+                                   ],
+                             index=['Ihr Szenario', 'Status quo'])
+    },
+]
+
+results_charts_tab3_viz = [results.ResultChart(
+    setup_labels=chart['setup_labels'],
+    type=chart['type'],
+    data=chart['data']).visualize(renderTo=chart['container_id'])
+                           for chart in results_charts_tab3]
+
+####################
+### Result Tab 4 ###
+####################
+
+results_charts_tab4 = [
     {
         'container_id': 'hc_res_wind_time',
         'type': 'line',
@@ -147,7 +164,8 @@ results_charts_tab3 = [
             'subtitle': {'text': 'in GW'},
             'yAxis': {'title': {'text': 'GW'}}
         },
-        'data_labels': ['Wind']
+        'data': {'Windenergie': [1, 2, 3, 4, 5, 4, 3, 2, 1, 8, 0],
+                 'Windenergie2': [8, 3, 3, 6, 2, 2, 1, 5, 5, 8, 7]}
     },
     {
         'container_id': 'hc_res_pv_time',
@@ -157,7 +175,7 @@ results_charts_tab3 = [
             'subtitle': {'text': 'in GW'},
             'yAxis': {'title': {'text': 'GW'}}
         },
-        'data_labels': ['PV']
+        'data': {'PV': [1, 2, 3, 4, 5, 4, 3, 2, 1, 8, 0]}
     },
     {
         'container_id': 'hc_res_bio_time',
@@ -167,12 +185,47 @@ results_charts_tab3 = [
             'subtitle': {'text': 'in GW'},
             'yAxis': {'title': {'text': 'GW'}}
         },
-        'data_labels': ['Biomasse', 'Biogas']
+        'data': {'Bio': [1, 2, 3, 4, 5, 4, 3, 2, 1, 8, 0]}
     }
 ]
 
-results_charts_tab3_viz = [results.ResultAnalysisVisualization(
+results_charts_tab4_viz = [results.ResultChart(
     setup_labels=chart['setup_labels'],
-    data_labels=chart['data_labels'],
-    type=chart['type']).visualize(renderTo=chart['container_id'])
-                   for chart in results_charts_tab3]
+    type=chart['type'],
+    data=pd.DataFrame(data=chart['data'],
+                      index=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'])).visualize(renderTo=chart['container_id'])
+                           for chart in results_charts_tab4]
+
+
+
+# StackedGroupedColumnChart: used in popups as placeholder charts
+results_charts_tab5 = [
+    {
+        'setup_labels': {
+            'title': {'text': 'Erzeugung'},
+            'subtitle': {'text': 'in GW'},
+            'yAxis': {'title': {'text': 'GW'}}
+        },
+        # 'data_labels': ['Anhalt', 'Bitterfeld', 'Wolfen', 'Dessau', 'Zerbst']
+    },
+    {
+        'setup_labels': {
+            'title': {'text': 'Bedarf'},
+            'subtitle': {'text': 'in GW'},
+            'yAxis': {'title': {'text': 'GW'}}
+        },
+        # 'data_labels': ['Anhalt', 'Bitterfeld', 'Wolfen', 'Dessau', 'Zerbst']
+    },
+    {
+        'setup_labels': {
+            'title': {'text': 'Erneuerbare Energien'},
+            'subtitle': {'text': 'in GW'},
+            'yAxis': {'title': {'text': 'GW'}}
+        },
+        # 'data_labels': ['Anhalt', 'Bitterfeld', 'Wolfen', 'Dessau', 'Zerbst']
+    }
+]
+results_charts_tab5_viz = [results.ResultChart(
+    setup_labels=l['setup_labels'],
+    #data_labels=l['data_labels'],
+    type='column').visualize() for l in results_charts_tab5]
