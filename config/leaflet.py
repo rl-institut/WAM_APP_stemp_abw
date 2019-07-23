8872cc34d3066473653a73f5ed6938ee045d56eb
nesnoj <jonathan.amme@rl-institut.de>
2019-07-23 09:38:51 +0200

    add correct mapbox attribution, use blank targets, noref and noopener

--- config/leaflet.py
+++ config/leaflet.py
@@ -6,25 +6,25 @@
 
 LEAFLET_CONFIG = {
     'TILES': [('Streets', 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
-               {'attribution': '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+               {'attribution': 'Tiles: &copy; <a href="https://www.mapbox.com/about/maps/" target="_blank" rel="noopener noreferrer">mapbox</a>, Data: &copy; <a href="http://openstreetmap.org" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors (<a href="https://www.mapbox.com/map-feedback/#/-74.5/40/10" target="_blank" rel="noopener noreferrer">improve this map</a>), <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank" rel="noopener noreferrer">CC-BY-SA</a>',
                 'id': 'mapbox.streets',
                 'minZoom': 9,
                 'maxZoom': 12,
                 }
                ),
               ('OSM B&W', 'http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png',
-               {'attribution': '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+               {'attribution': '&copy; <a href="http://openstreetmap.org" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank" rel="noopener noreferrer">CC-BY-SA</a>',
                 'minZoom': 9,
                 'maxZoom': 12,
                 }
                ),
               ('OSM', 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-               {'attribution': '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+               {'attribution': '&copy; <a href="http://openstreetmap.org" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank" rel="noopener noreferrer">CC-BY-SA</a>',
                 'minZoom': 9,
                 'maxZoom': 12,
                 }),
               ('OpenTopoMap', 'http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
-               {'attribution': 'Data: &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Display: &copy <a href="http://opentopomap.org">OpenTopoMap</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+               {'attribution': 'Tiles: &copy <a href="http://opentopomap.org" target="_blank" rel="noopener noreferrer">OpenTopoMap</a>, Data: &copy; <a href="http://openstreetmap.org" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank" rel="noopener noreferrer">CC-BY-SA</a>',
                 'minZoom': 9,
                 'maxZoom': 12,
                 })
