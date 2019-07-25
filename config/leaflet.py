b5c0a18b79f899e746876f74296c65c906617a00
Alexis Michaltsis <alexis.michaltsis@rl-institut.de>
2019-07-25 15:13:59 +0200

    Add wam-rli mapbox token #67

--- config/leaflet.py
+++ config/leaflet.py
@@ -5,7 +5,7 @@
 # Related issue: https://github.com/rl-institut/WAM/issues/74
 
 LEAFLET_CONFIG = {
-    'TILES': [('Streets', 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+    'TILES': [('Streets', 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoid2FtLXJsaSIsImEiOiJjanlpbTdsdXgwMHljM2JueGNrODZtbjB5In0.A7X6_rpw7hRehkulqMjpXw',
                {'attribution': 'Tiles: &copy; <a href="https://www.mapbox.com/about/maps/" target="_blank" rel="noopener noreferrer">mapbox</a>, Data: &copy; <a href="http://openstreetmap.org" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors (<a href="https://www.mapbox.com/map-feedback/#/-74.5/40/10" target="_blank" rel="noopener noreferrer">improve this map</a>), <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank" rel="noopener noreferrer">CC-BY-SA</a>',
                 'id': 'mapbox.streets',
                 'minZoom': 9,
