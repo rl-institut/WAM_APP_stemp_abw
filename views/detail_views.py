2574cbd337f8145c1e1ef256ce4172d1c2c63f91
Alexis Michaltsis <alexis.michaltsis@rl-institut.de>
2019-09-11 18:19:45 +0200

    Fix typo

--- views/detail_views.py
+++ views/detail_views.py
@@ -1091,7 +1091,7 @@ class RegMunDemElEnergyPerCapitaResultDetailView(MasterDetailView):
         # Convert data to appropriate format for pie chart
         data = data.reset_index().to_dict(orient='records')
         setup_labels = {
-            'title': {'text': str(_('Eregbnis: Strombedarf'))},
+            'title': {'text': str(_('Ergebnis: Strombedarf'))},
             'subtitle': {'text': str(_('je EinwohnerIn nach Verbrauchergruppe'))},
             'plotOptions': {
                 'pie': {
