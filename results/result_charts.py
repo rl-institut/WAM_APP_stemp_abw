3c9226e213d376404e31ac03b13cd92638f531ef
Alexis Michaltsis <alexis.michaltsis@rl-institut.de>
2019-09-12 12:10:40 +0200

    Make result charts 1 and 2 callable

--- results/result_charts.py
+++ results/result_charts.py
@@ -8,135 +8,156 @@ import pandas as pd
 ### Result Tab 1 ###
 ####################
 
-results_charts_tab1 = [
-    {
-        'container_id': 'hc_column_power_prod_both_scn',
-        'type': 'column',
-        'setup_labels': {
-            'title': {'text': str(_('Stromerzeugung'))},
-            'subtitle': {'text': str(_('in GWh'))},
-            'yAxis': {'title': {'text': str(_('GWh'))}},
-            'plotOptions': {'column': {'stacking': 'normal'}}
+def results_charts_tab1():
+    return [
+        {
+            'container_id': 'hc_column_power_prod_both_scn',
+            'type': 'column',
+            'setup_labels': {
+                'title': {'text': str(_('Stromerzeugung'))},
+                'subtitle': {'text': str(_('in GWh'))},
+                'yAxis': {'title': {'text': str(_('GWh'))}},
+                'plotOptions': {'column': {'stacking': 'normal'}}
+            },
+            'data': pd.DataFrame(data=[{str(_('Windenergie')): 0,
+                                        str(_('PV Freifläche')): 0,
+                                        str(_('PV Dach')): 0,
+                                        str(_('Wasserkraft')): 0,
+                                        str(_('Biogas')): 0,
+                                        str(_('Fossile Brennstoffe')): 0,
+                                        str(_('Import')): 0},
+                                       {str(_('Windenergie')): 0,
+                                        str(_('PV Freifläche')): 0,
+                                        str(_('PV Dach')): 0,
+                                        str(_('Wasserkraft')): 0,
+                                        str(_('Biogas')): 0,
+                                        str(_('Fossile Brennstoffe')): 0,
+                                        str(_('Import')): 0}
+                                       ],
+                                 index=[str(_('Ihr Szenario')),
+                                        str(_('Status quo'))])
         },
-        'data': pd.DataFrame(data=[{str(_('Windenergie')): 0,
-                                    str(_('PV Freifläche')): 0,
-                                    str(_('PV Dach')): 0,
-                                    str(_('Wasserkraft')): 0,
-                                    str(_('Biogas')): 0,
-                                    str(_('Fossile Brennstoffe')): 0,
-                                    str(_('Import')): 0},
-                                   {str(_('Windenergie')): 0,
-                                    str(_('PV Freifläche')): 0,
-                                    str(_('PV Dach')): 0,
-                                    str(_('Wasserkraft')): 0,
-                                    str(_('Biogas')): 0,
-                                    str(_('Fossile Brennstoffe')): 0,
-                                    str(_('Import')): 0}
-                                   ],
-                             index=[str(_('Ihr Szenario')), str(_('Status quo'))])
-    },
-    {
-        'container_id': 'hc_column_power_dem_both_scn',
-        'type': 'column',
-        'setup_labels': {
-            'title': {'text': str(_('Stromverbrauch'))},
-            'subtitle': {'text': str(_('in GWh'))},
-            'yAxis': {'title': {'text': str(_('GWh'))}},
-            'plotOptions': {'column': {'stacking': 'normal'}}
+        {
+            'container_id': 'hc_column_power_dem_both_scn',
+            'type': 'column',
+            'setup_labels': {
+                'title': {'text': str(_('Stromverbrauch'))},
+                'subtitle': {'text': str(_('in GWh'))},
+                'yAxis': {'title': {'text': str(_('GWh'))}},
+                'plotOptions': {'column': {'stacking': 'normal'}}
+            },
+            'data': pd.DataFrame(data=[{str(_('Haushalte')): 0,
+                                        str(_('GHD')): 0,
+                                        str(_('Industrie')): 0,
+                                        str(_('Export')): 0},
+                                       {str(_('Haushalte')): 0,
+                                        str(_('GHD')): 0,
+                                        str(_('Industrie')): 0,
+                                        str(_('Export')): 0}
+                                       ],
+                                 index=[str(_('Ihr Szenario')),
+                                        str(_('Status quo'))])
         },
-        'data': pd.DataFrame(data=[{str(_('Haushalte')): 0,
-                                    str(_('GHD')): 0,
-                                    str(_('Industrie')): 0,
-                                    str(_('Export')): 0},
-                                   {str(_('Haushalte')): 0,
-                                    str(_('GHD')): 0,
-                                    str(_('Industrie')): 0,
-                                    str(_('Export')): 0}
-                                   ],
-                             index=[str(_('Ihr Szenario')), str(_('Status quo'))])
-    },
-    {
-        'container_id': 'hc_column_power_own_cons_both_scn',
-        'type': 'column',
-        'setup_labels': {
-            'title': {'text': str(_('Eigenversorgung Strom'))},
-            'subtitle': {'text': str(_('in %'))},
-            'yAxis': {'title': {'text': '%'}}
-        },
-        'data': pd.DataFrame(data=[{str(_('Bilanziell')): 0,
-                                    str(_('Zeitgleich')): 0},
-                                   {str(_('Bilanziell')): 0,
-                                    str(_('Zeitgleich')): 0}
-                                   ],
-                             index=[str(_('Ihr Szenario')), str(_('Status quo'))])
-    }
-]
+        {
+            'container_id': 'hc_column_power_own_cons_both_scn',
+            'type': 'column',
+            'setup_labels': {
+                'title': {'text': str(_('Eigenversorgung Strom'))},
+                'subtitle': {'text': str(_('in %'))},
+                'yAxis': {'title': {'text': '%'}}
+            },
+            'data': pd.DataFrame(data=[{str(_('Bilanziell')): 0,
+                                        str(_('Zeitgleich')): 0},
+                                       {str(_('Bilanziell')): 0,
+                                        str(_('Zeitgleich')): 0}
+                                       ],
+                                 index=[str(_('Ihr Szenario')),
+                                        str(_('Status quo'))])
+        }
+    ]
 
-results_charts_tab1_viz = [results.ResultChart(
-    setup_labels=chart['setup_labels'],
-    type=chart['type'],
-    data=chart['data']).visualize(renderTo=chart['container_id'])
-                           for chart in results_charts_tab1]
+
+def results_charts_tab1_viz():
+    return [
+        results.ResultChart(
+            setup_labels=chart['setup_labels'],
+            type=chart['type'],
+            data=chart['data']).visualize(renderTo=chart['container_id'])
+        for chart in results_charts_tab1()
+    ]
+
 
 ####################
 ### Result Tab 2 ###
 ####################
 
-results_charts_tab2 = [
-    {
-        'container_id': 'hc_pie_power_production_user_scn',
-        'type': 'pie',
-        'setup_labels': {
-            'title': {'text': str(_('Zusammensetzung Stromerzeugung (Dein Szenario)'))},
-            'subtitle': {'text': str(_('in GWh'))},
-            'yAxis': {'title': {'text': str(_('GWh'))}}
+def results_charts_tab2():
+    return [
+        {
+            'container_id': 'hc_pie_power_production_user_scn',
+            'type': 'pie',
+            'setup_labels': {
+                'title': {'text': str(
+                    _('Zusammensetzung Stromerzeugung (Dein Szenario)'))},
+                'subtitle': {'text': str(_('in GWh'))},
+                'yAxis': {'title': {'text': str(_('GWh'))}}
+            },
+            'data': pd.DataFrame(data={'name': [str(_('Windenergie')),
+                                                str(_('PV Freifläche')),
+                                                str(_('PV Dach')),
+                                                str(_('Wasserkraft')),
+                                                str(_('Biogas')),
+                                                str(_('Fossile Brennstoffe')),
+                                                str(_('Import'))],
+                                       'y': [0] * 7}).reset_index().to_dict(
+                orient='records')
         },
-        'data': pd.DataFrame(data={'name': [str(_('Windenergie')),
-                                            str(_('PV Freifläche')),
-                                            str(_('PV Dach')),
-                                            str(_('Wasserkraft')),
-                                            str(_('Biogas')),
-                                            str(_('Fossile Brennstoffe')),
-                                            str(_('Import'))],
-                                   'y': [0] * 7}).reset_index().to_dict(orient='records')
-    },
-    {
-        'container_id': 'hc_pie_power_production_sq_scn',
-        'type': 'pie',
-        'setup_labels': {
-            'title': {'text': str(_('Zusammensetzung Stromerzeugung (Status quo)'))},
-            'subtitle': {'text': str(_('in GWh'))},
-            'yAxis': {'title': {'text': str(_('GWh'))}}
+        {
+            'container_id': 'hc_pie_power_production_sq_scn',
+            'type': 'pie',
+            'setup_labels': {
+                'title': {'text': str(
+                    _('Zusammensetzung Stromerzeugung (Status quo)'))},
+                'subtitle': {'text': str(_('in GWh'))},
+                'yAxis': {'title': {'text': str(_('GWh'))}}
+            },
+            'data': pd.DataFrame(data={'name': [str(_('Windenergie')),
+                                                str(_('PV Freifläche')),
+                                                str(_('PV Dach')),
+                                                str(_('Wasserkraft')),
+                                                str(_('Biogas')),
+                                                str(_('Fossile Brennstoffe')),
+                                                str(_('Import'))],
+                                       'y': [0] * 7}).reset_index().to_dict(
+                orient='records')
         },
-        'data': pd.DataFrame(data={'name': [str(_('Windenergie')),
-                                            str(_('PV Freifläche')),
-                                            str(_('PV Dach')),
-                                            str(_('Wasserkraft')),
-                                            str(_('Biogas')),
-                                            str(_('Fossile Brennstoffe')),
-                                            str(_('Import'))],
-                                   'y': [0] * 7}).reset_index().to_dict(orient='records')
-    },
-    {
-        'container_id': 'hc_column_power_prod_m_user_scn',
-        'type': 'column',
-        'setup_labels': {
-            'title': {'text': str(_('Monatliche Stromerzeugung aus EE (Dein Szenario)'))},
-            'subtitle': {'text': str(_('in GWh'))},
-            'yAxis': {'title': {'text': str(_('GWh'))}}
+        {
+            'container_id': 'hc_column_power_prod_m_user_scn',
+            'type': 'column',
+            'setup_labels': {
+                'title': {'text': str(
+                    _('Monatliche Stromerzeugung aus EE (Dein Szenario)'))},
+                'subtitle': {'text': str(_('in GWh'))},
+                'yAxis': {'title': {'text': str(_('GWh'))}}
 
+            },
+            'data': pd.DataFrame(
+                data={str(_('Windenergie')): [0 for _ in MONTH_LABELS],
+                      str(_('PV')): [0 for _ in MONTH_LABELS]},
+                index=MONTH_LABELS)
         },
-        'data': pd.DataFrame(data={str(_('Windenergie')): [0 for _ in MONTH_LABELS],
-                                   str(_('PV')): [0 for _ in MONTH_LABELS]},
-                             index=MONTH_LABELS)
-    },
-]
+    ]
 
-results_charts_tab2_viz = [results.ResultChart(
-    setup_labels=chart['setup_labels'],
-    type=chart['type'],
-    data=chart['data']).visualize(renderTo=chart['container_id'])
-                           for chart in results_charts_tab2]
+
+def results_charts_tab2_viz():
+    return [
+        results.ResultChart(
+            setup_labels=chart['setup_labels'],
+            type=chart['type'],
+            data=chart['data']).visualize(renderTo=chart['container_id'])
+        for chart in results_charts_tab2()
+    ]
+
 
 ####################
 ### Result Tab 3 ###
@@ -160,7 +181,8 @@ results_charts_tab3 = [
                                     str(_('Industrie')): 0,
                                     str(_('Export')): 0}
                                    ],
-                             index=[str(_('Ihr Szenario')), str(_('Status quo'))])
+                             index=[str(_('Ihr Szenario')),
+                                    str(_('Status quo'))])
     },
 ]
 
@@ -212,9 +234,9 @@ results_charts_tab4_viz = [results.ResultChart(
     setup_labels=chart['setup_labels'],
     type=chart['type'],
     data=pd.DataFrame(data=chart['data'],
-                      index=MONTH_LABELS)).visualize(renderTo=chart['container_id'])
+                      index=MONTH_LABELS)).visualize(
+    renderTo=chart['container_id'])
                            for chart in results_charts_tab4]
-
 
 # StackedGroupedColumnChart: used in popups as placeholder charts
 results_charts_tab5 = [
