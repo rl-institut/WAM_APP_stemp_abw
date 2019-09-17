5f4e60d1af250336cd36bc2fcba345abd75a941b
nesnoj <jonathan.amme@rl-institut.de>
2019-09-17 23:21:44 +0200

    rm DEFAULT_LANGUAGE from app #72
    as it's now set in WAM settings, cf. https://github.com/rl-institut/WAM/issues/76

--- settings.py
+++ settings.py
@@ -1,18 +1,9 @@
 # The settings in this file will be global project settings in wam.settings
 
-from stemp_abw.app_settings import DEFAULT_LANGUAGE
-
-
 INSTALLED_APPS = [
     'leaflet',
     'djgeojson',
 ]
-
-# Default project language (for available app languages see LANGUAGE_STORE in
-# app_settings.py). If this is not present i18n in stemp_abw app will not work.
-# This might overwrite the default language of other apps, if the WAM project
-# base is combined with other WAM apps that use a different default language.
-LANGUAGE_CODE = DEFAULT_LANGUAGE
 
 # Leaflet config: variable "LEAFLET_CONFIG" moved to config/leaflet.py since it
 # conflicts with other apps using django-leaflet package.
